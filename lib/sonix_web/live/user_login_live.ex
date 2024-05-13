24c72f24bebdd0eda3f7536f86872777502405a1
zvlasic <zlayo1@gmail.com>
2024-05-13 18:26:47 +0200

    Improve header looks

--- lib/sonix_web/live/user_login_live.ex
+++ lib/sonix_web/live/user_login_live.ex
@@ -1,18 +1,11 @@
 defmodule SonixWeb.UserLoginLive do
   use SonixWeb, :live_view
 
-  alias Sonix.LastFmClient
-
   def mount(_params, _session, socket), do: {:ok, socket}
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.button id="auth" phx-click="auth">Log in with Last.FM</.button>
-    </div>
+    <div class="mx-auto max-w-sm"></div>
     """
   end
-
-  def handle_event("auth", _, socket),
-    do: {:noreply, redirect(socket, external: LastFmClient.oauth_url())}
 end
