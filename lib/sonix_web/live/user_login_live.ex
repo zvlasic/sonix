74ace01d1a5fdf24cf9eece02377f44cdaf02965
zvlasic <zlayo1@gmail.com>
2024-05-14 08:57:06 +0200

    Rework login screen

--- lib/sonix_web/live/user_login_live.ex
+++ lib/sonix_web/live/user_login_live.ex
@@ -1,11 +1,18 @@
 defmodule SonixWeb.UserLoginLive do
   use SonixWeb, :live_view
+  alias Sonix.LastFmClient
 
   def mount(_params, _session, socket), do: {:ok, socket}
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm"></div>
+    <div class="flex flex-col items-center justify-center">
+      <img src={~p"/images/logo.png"} />
+      <.button id="auth" phx-click="oauth">Login using Last.FM</.button>
+    </div>
     """
   end
+
+  def handle_event("oauth", _value, socket),
+    do: {:noreply, redirect(socket, external: LastFmClient.oauth_url())}
 end
